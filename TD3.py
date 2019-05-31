6df46710868f35a48140d5745fe8657673fd22de
zhan0903 <z.han0903@qq.com>
2019-05-31 11:50:21 +1000

    debug

--- TD3.py
+++ TD3.py
@@ -125,7 +125,7 @@ class TD3(object):
 			with self.timers["update_critic"]:
 				# Select action according to policy and add clipped noise 
 				noise = torch.FloatTensor(u).data.normal_(0, policy_noise).to(device)
-			noise = noise.clamp(-noise_clip, noise_clip)
+				noise = noise.clamp(-noise_clip, noise_clip)
 			next_action = (self.actor_target(next_state) + noise).clamp(-self.max_action, self.max_action)
 
 			# Compute the target Q value
